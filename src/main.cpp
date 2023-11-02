45bc7c9cd7cdfc4f3979a578f591767c1596ffb7
Kaguya <kaguya@Kaguya.cn>
2023-11-02 09:01:47 +0800

    因为int被爆了就全用long long了

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,29 @@
 #include <map>
 using namespace std;
 
+using ll = int_fast64_t;
+vector<ll> utf8_tokenize(const string &str)
+{
+    vector<ll> split;
+    auto len = str.length();
+    auto left = 0;
+    auto right = 1;
+
+    for (auto i = 0; i < len; i++)
+    {
+        if (right >= len || ((str[right] & 0xc0) != 0x80))
+        {
+            ll s = 0;
+            for (auto i = left; i < right; i++)
+                s = (s << 8) + (unsigned char)str[i];
+            split.emplace_back(s);
+            left = right;
+        }
+        ++right;
+    }
+    return split;
+}
+
 vector<string> utf8_split(const string &str)
 {
     vector<string> split;
@@ -62,7 +85,7 @@ int lcs_sequence_length(const string &str1, const string &str2)
 }
 
 // 我们用到的核心代码，我只优化这个
-vector<int> lcs_sequence_idx(const string &str, const string &ref)
+vector<ll> lcs_sequence_idx(const string &str, const string &ref)
 {
     /***
      * Hunt-Szymanski Algorithm for LCS
@@ -72,29 +95,28 @@ vector<int> lcs_sequence_idx(const string &str, const string &ref)
      *
      * copied and modified from https://github.com/sgtlaugh/algovault/blob/0ba0b3eb0d2e31e78de05da188f13d8d7d0365ae/code_library/hunt_szymanski.cpp
      ***/
-    vector<string> A = utf8_split(str);
-    vector<string> B = utf8_split(ref);
+    auto A = utf8_tokenize(str);
+    auto B = utf8_tokenize(ref);
 
-    unordered_map<string, vector<int>> adj; // 均摊O(B.size())，当然实际可能会大一点但是绝对不会达到A*B
-    int i, j, n = A.size(), m = B.size();
+    unordered_map<ll, vector<ll>> adj; // 均摊O(B.size())，当然实际可能会大一点但是绝对不会达到A*B
+    ll i, j, n = A.size(), m = B.size();
     if (m == 0 || n == 0)
-        return vector<int>(A.size(), -1);
+        return vector<ll>(A.size(), -1);
 
     for (i = 0; i < m; i++)
         adj[B[i]].push_back(i); // adj就是论文中的MATCHLIST，这里是升序放进去，我们之后倒序遍历来实现降序访问
 
-    vector<int> thresh{-1};       // 论文中的THRESH数组
+    vector<ll> thresh{-1};        // 论文中的THRESH数组
     thresh.reserve(A.size() + 1); // 放入虚点，避免傻逼讨论
-
     // 实现自论文https://cse.hkust.edu.hk/mjg_lib/bibs/DPSu/DPSu.Files/HuSz77.pdf，用两个vector来实现单前向节点链表
-    vector<int> link{0}; // 记前向node号，和thresh数组等长
+    vector<ll> link{0}; // 记前向node号，和thresh数组等长
     link.reserve(A.size() + 1);
 
     struct listnode
     {
-        int b_index;
-        int prv; // 指向linklistnode中上一个元素的下标，考虑到指针会大一倍，这里不用指针
-        listnode(int a, int b) : b_index(a), prv(b) {}
+        ll b_index;
+        ll prv; // 指向linklistnode中上一个元素的下标，考虑到指针会大一倍，这里不用指针
+        listnode(ll a, ll b) : b_index(a), prv(b) {}
     };
 
     vector<listnode> linklistnode{listnode{-1, -1}}; // 这个是节点内容，长度等于A和B中元素的匹配数，也就是说最坏还是n^2的，但实际数据远远达不到
@@ -102,9 +124,9 @@ vector<int> lcs_sequence_idx(const string &str, const string &ref)
 
     for (i = 0; i < n; i++)
     {
-        for (j = (int)adj[A[i]].size() - 1; j >= 0; j--) // 对于英文字符来说，这种方法不一定很优，因为每个adj的桶里的内容都很多，但是对于中文字符来说这种做法很可能很快，常用的文字有3500个，相当于有3500个桶来匀全文的匹配
+        for (j = adj[A[i]].size() - 1; j >= 0; j--) // 对于英文字符来说，这种方法不一定很优，因为每个adj的桶里的内容都很多，但是对于中文字符来说这种做法很可能很快，常用的文字有3500个，相当于有3500个桶来匀全文的匹配
         {
-            int x = adj[A[i]][j];
+            auto x = adj[A[i]][j];
             if (x > thresh.back())
             {
                 thresh.emplace_back(x);
@@ -114,8 +136,8 @@ vector<int> lcs_sequence_idx(const string &str, const string &ref)
             }
             else
             {
-                int insert_pos = lower_bound(thresh.begin(), thresh.end(), x) - thresh.begin(); // thresh虽然冗余但是去掉会需要写比较tricky的lambda，影响性能
-                thresh[insert_pos] = x;                                                         // 最坏情况出现在A串和B串都由同一个字符构造而来，这种情况是n*m*log(n)的时间，但是在我们的数据上不可能发生
+                auto insert_pos = lower_bound(thresh.begin(), thresh.end(), x) - thresh.begin(); // thresh虽然冗余但是去掉会需要写比较tricky的lambda，影响性能
+                thresh[insert_pos] = x;                                                          // 最坏情况出现在A串和B串都由同一个字符构造而来，这种情况是n*m*log(n)的时间，但是在我们的数据上不可能发生
 
                 linklistnode.emplace_back(x, link[insert_pos - 1]);
                 link[insert_pos] = linklistnode.size() - 1;
@@ -137,7 +159,6 @@ vector<int> lcs_sequence_idx(const string &str, const string &ref)
             j = linklistnode[j].prv;
         }
     }
-
     return thresh;
 }
 /*
@@ -446,7 +467,6 @@ vector<int> levenshtein_distance_of_list(const string &str1, vector<string> &str
     return ls;
 }
 
-
 // signed main()
 // {
 //     // vector<int>ls = lcs_sequence_idx("ABC123IJKL#O#TUVWXY", "ABCdefghIjkLmnOpqrsTUvWxY");
@@ -457,31 +477,32 @@ vector<int> levenshtein_distance_of_list(const string &str1, vector<string> &str
 // }
 
 namespace py = pybind11;
-PYBIND11_MODULE(pylcs, m) {
+PYBIND11_MODULE(pylcs, m)
+{
     m.def("lcs", &lcs_sequence_length, R"pbdoc(Longest common subsequence)pbdoc");
     m.def("lcs_sequence_length", &lcs_sequence_length, R"pbdoc(Longest common subsequence)pbdoc");
     m.def("lcs_sequence_idx", &lcs_sequence_idx, R"pbdoc(Longest common subsequence indices mapping from str to ref)pbdoc",
-        py::arg("s"), py::arg("ref"));
+          py::arg("s"), py::arg("ref"));
     m.def("lcs_of_list", &lcs_sequence_of_list, R"pbdoc(Longest common subsequence of list)pbdoc");
     m.def("lcs_sequence_of_list", &lcs_sequence_of_list, R"pbdoc(Longest common subsequence of list)pbdoc");
 
     m.def("lcs2", &lcs_string_length, R"pbdoc(Longest common substring)pbdoc");
     m.def("lcs_string_length", &lcs_string_length, R"pbdoc(Longest common substring)pbdoc");
     m.def("lcs_string_idx", &lcs_string_idx, R"pbdoc(Longest common substring indices mapping from str to ref)pbdoc",
-        py::arg("s"), py::arg("ref"));
+          py::arg("s"), py::arg("ref"));
     m.def("lcs2_of_list", &lcs_string_of_list, R"pbdoc(Longest common substring of list)pbdoc");
     m.def("lcs_string_of_list", &lcs_string_of_list, R"pbdoc(Longest common substring of list)pbdoc");
 
     m.def("levenshtein_distance", &levenshtein_distance, R"pbdoc(Levenshtein Distance of Two Strings)pbdoc");
     m.def("levenshtein_distance", &levenshtein_distance_weighted, R"pbdoc(Levenshtein Distance of Two Strings. A weight dict<str, dict<str, float>> can be used.)pbdoc",
-        py::arg("str1"), py::arg("str2"), py::arg("weight"));
+          py::arg("str1"), py::arg("str2"), py::arg("weight"));
     m.def("levenshtein_distance_idx", &levenshtein_distance_idx_weighted, R"pbdoc(Levenshtein Distance indices mapping from str to ref)pbdoc",
-        py::arg("s"), py::arg("ref"), py::arg("weight")=map<string, map<string, float>>());
+          py::arg("s"), py::arg("ref"), py::arg("weight") = map<string, map<string, float>>());
     m.def("edit_distance", &levenshtein_distance, R"pbdoc(Same As levenshtein_distance(): Levenshtein Distance of Two Strings)pbdoc");
     m.def("edit_distance", &levenshtein_distance_weighted, R"pbdoc(Same As levenshtein_distance(): Levenshtein Distance of Two Strings)pbdoc",
-        py::arg("str1"), py::arg("str2"), py::arg("weight"));
+          py::arg("str1"), py::arg("str2"), py::arg("weight"));
     m.def("edit_distance_idx", &levenshtein_distance_idx_weighted, R"pbdoc(Edit Distance indices mapping from str to ref)pbdoc",
-        py::arg("s"), py::arg("ref"), py::arg("weight")=map<string, map<string, float>>());
+          py::arg("s"), py::arg("ref"), py::arg("weight") = map<string, map<string, float>>());
     m.def("levenshtein_distance_of_list", &levenshtein_distance_of_list, R"pbdoc(Levenshtein Distance of one string to a list of strings)pbdoc");
     m.def("edit_distance_of_list", &levenshtein_distance_of_list, R"pbdoc(Levenshtein Distance of one string to a list of strings)pbdoc");
 }
